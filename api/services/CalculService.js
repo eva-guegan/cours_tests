9ff30d61f7c08f286767b986b3148eab8bb8fc4d
eva.guegan <eva.guegan@hotmail.fr>
2021-03-08 11:19:55 +0100

    first commit7

---
+++ api/services/CalculService.js
@@ -0,0 +1,14 @@
+module.exports = {
+  addition : (a, b) => {
+    return a+b;
+  },
+  soustraction : (a, b) => {
+    return a-b;
+  },
+  multiplication : (a, b) => {
+    return a*b;
+  },
+  division : (a, b) => {
+    return a/b;
+  },
+};
