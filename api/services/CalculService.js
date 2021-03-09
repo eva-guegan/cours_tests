93705d1eb172e37434232d86a69b7b76e5bc5492
eva.guegan <eva.guegan@hotmail.fr>
2021-03-09 10:06:57 +0100

    mise en place de mocks

--- api/services/CalculService.js
+++ api/services/CalculService.js
@@ -1,17 +1,33 @@
+const {throws} = require('assert');
+//
 module.exports = {
   addition : (a, b) => {
     return a+b;
   },
+  //
   soustraction : (a, b) => {
     return a-b;
   },
+  //
   multiplication : (a, b) => {
     return a*b;
   },
+  //
   division : (a, b) => {
     if (b === 0) {
-      return false;
+      throw new Error('division par 0 impossible');
     }
     return a/b;
+  },
+  //
+  pourcentage : (a, pourct) => {
+    // avec a = valeur partielle et b = valeur totale
+    let result = CalculService.multiplication(a, pourct);
+    result = CalculService.division(result, 100);
+    return result+1;
+  },
+  //
+  square : (a) => {
+    return CalculService.multiplication(a, a);
   },
 };
--- tests/fixture/services/CalculService.test.js
+++ tests/fixture/services/CalculService.test.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
+var sinon = require('sinon');
 
 describe('CalculService', function () {
+  //
   describe('#addition', function () {
     it('should add values', function () {
       let result = CalculService.addition(2, 2);
@@ -24,8 +26,63 @@ describe('CalculService', function () {
 
   describe('#division', function () {
     it('should div values', function () {
-      let result = CalculService.division(10, 0);
+      let result = CalculService.division(10, 2);
       assert.equal(result, 5);
     });
+    //
+    it('shouldn\'t div by 0', function () {
+      try {
+        CalculService.division(10, 0);
+      } catch (e) {
+        assert.equal(e.message, 'division par 0 impossible');
+        return;
+      }
+      // si ne passe pas dans l'exception alors le test échoue
+      assert(false);
+    });
   });
+  //
+  describe('#pourcentage', function () {
+    it('should calcul % of a value', function () {
+      let stub = sinon.stub(CalculService, 'multiplication').callsFake(function () {return 2500;});
+      let stub2 = sinon.stub(CalculService, 'division').callsFake(function () {return 25;});
+      //
+      let result = CalculService.pourcentage(50, 100);
+      //
+      CalculService.multiplication.restore();
+      CalculService.division.restore();
+      //
+      assert.equal(result, 26);
+    });
+    it('should call once each method', function () {
+      // créer un stub tout seul
+      const spy1 = sinon.spy(CalculService, "multiplication");
+      const spy2 = sinon.spy(CalculService, "division");
+      //
+      CalculService.pourcentage(50, 100);
+      //
+      assert(spy1.calledOnce);
+      assert(spy2.calledOnce);
+      //
+      CalculService.multiplication.restore();
+      CalculService.division.restore();
+    });
+  });
+  //
+  describe('#square', function () {
+    it('should calcul the square', function () {
+      // créer un stub tout seul
+      const spy1 = sinon.spy(CalculService, "multiplication");
+      const spy2 = sinon.spy(CalculService, "division");
+      //
+      CalculService.square(3);
+      //
+      assert(spy1.calledOnce);
+      assert.equal(spy2.callCount, 0);
+      //
+      CalculService.multiplication.restore();
+      CalculService.division.restore();
+    });
+  });
+  //
 });
